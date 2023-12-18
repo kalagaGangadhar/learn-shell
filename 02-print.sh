bb39a9ba1e5edd049d10e5b343862c11ed22d568
Gangadhar <gangadharkalaga@gmail.com>
2023-12-18 18:12:58 +0530

    echo colors

---
+++ 02-print.sh
@@ -0,0 +1,18 @@
+echo hello world
+
+#red        31
+#green      32
+#yellow     33
+#magenta    34
+#blue       35
+#cyan       36
+
+
+#echo -e "\e[CODEmMESSAGE\e[0m"
+
+echo -e "\e[31mHello world\e[0m"
+echo -e "\e[32mHello world\e[0m"
+echo -e "\e[33mHello world\e[0m"
+echo -e "\e[34mHello world\e[0m"
+echo -e "\e[35mHello world\e[0m"
+echo -e "\e[36mHello world\e[0m"
